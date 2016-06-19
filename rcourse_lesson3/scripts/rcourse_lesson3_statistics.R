373ec382b07cd209c901a2fdfce132d9fc490be9
pcavatore <pcavatore@gmail.com>
2016-06-19 19:19:50 +0200

    Made statistics script.

---
+++ rcourse_lesson3/scripts/rcourse_lesson3_statistics.R
@@ -0,0 +1,42 @@
+rm(list=ls())
+
+## READ IN DATA ####
+source("scripts/rcourse_lesson3_cleaning.R")
+
+
+## LOAD PACKAGES ####
+# [none currently needed]
+
+
+## ORGANIZE DATA ####
+# Full season data
+data_stats = data_clean
+# data_stats = data_clean %>%
+#   # Change order of level of variables for figure
+#   mutate(allstar_break = factor(allstar_break, levels = c("before", "after")))
+
+
+# Player specific data
+data_posey_stats = data_posey_clean
+
+
+## BUILD MODEL - FULL SEASON DATA ####
+# Make logistic regression model
+allstar.glm = glm(win ~ allstar_break, family = "binomial", data = data_stats)
+
+# Summarise model and save
+allstar.glm_sum = summary(allstar.glm)
+
+# Call summary of model
+allstar.glm_sum
+
+
+## BUILD MODEL - PLAYER SPECIFIC DATA ####
+# Make logistic regression model
+posey_walked.glm = glm(win ~ walked, family = "binomial", data = data_posey_stats)
+
+# Summarise model and save
+posey_walked.glm_sum = summary(posey_walked.glm)
+
+# Call summary of model
+posey_walked.glm_sum
